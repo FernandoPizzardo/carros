f8694dd7b50d86535d2099f08d8d8e2bd4adcdd6
Fernando_Pizzardo <fpizzardo@brq.com>
2022-02-25 15:37:08 -0300

    seção 10 e melhoras no webservice

--- lib/pages/carro/carros_api.dart
+++ lib/pages/carro/carros_api.dart
@@ -2,10 +2,16 @@ import 'dart:convert';
 import 'carro.dart';
 import 'package:http/http.dart' as http;
 
+class tipoCarro {
+  static const String classicos = "classicos";
+  static const String esportivos = "esportivos";
+  static const String luxo = "luxo";
+}
+
 class CarrosApi {
-  static Future<List<Carro>> getCarros() async {
-    var uri =
-        Uri.parse('https://carros-springboot.herokuapp.com/api/v1/carros/');
+  static Future<List<Carro>> getCarros(String tipo) async {
+    var uri = Uri.parse(
+        'https://carros-springboot.herokuapp.com/api/v1/carros/tipo/$tipo');
     var response = await http.get(uri);
     String json = response.body;
     List list = jsonDecode(json);
--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -5,17 +5,45 @@ import 'package:flutter/material.dart';
 
 import 'carro.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Carros'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Carros'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: 'Clássicos',
+              ),
+              Tab(
+                text: 'Esportivos',
+              ),
+              Tab(
+                text: 'Luxo',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            CarrosListView(tipoCarro.classicos),
+            CarrosListView(tipoCarro.esportivos),
+            CarrosListView(tipoCarro.luxo),
+          ],
+        ),
+        drawer: DrawerList(),
       ),
-      body: CarrosListView(),
-      drawer: DrawerList(),
     );
   }
 }
